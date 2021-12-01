5f9cc28d064c3fab341fbb295262caf28ebd0eca
SigmaHyperon <nano_tech@web.de>
2021-12-01 23:04:22 +0100

    improve cli features

--- index.ts
+++ index.ts
@@ -1,15 +1,58 @@
 import yargs from "yargs";
 import { hideBin } from 'yargs/helpers'
+import path from "path";
+import Constants from "./lib/constants";
+import fs from "fs";
 
-const args = yargs(hideBin(process.argv))
-	.option("day", {
-		alias: "d",
-		type: "number"
-	})
-	.demandOption("day")
-	.argv;
+yargs(hideBin(process.argv))
+.option("day", {
+	alias: "d",
+	type: "number"
+})
+.command("*", false, () => {}, (args) => {
+	if(!(args instanceof Promise)) {
+		import(`./days/${formatDay(args.day)}`);
+	}
+})
+.command("init", "initialize day", () => {}, (args) => {
+	if(!(args instanceof Promise)) {
+		createCodeFile(args.day)
+	}
+})
+.argv;
 
-if(!(args instanceof Promise)) {
-	const day = args.day.toString().length > 1 ? args.day.toString() : `0${args.day}`;
-	import(`./days/${day}`);
+function formatDay(day: number): string {
+	return day.toString().length > 1 ? day.toString() : `0${day}`
+}
+
+function createCodeFile(day: number) {
+	const content = `
+		import { Input } from "../../lib/input";
+		import Solver from "../../lib/solver";
+		import "../../lib/prototypes";
+
+		const values = Input.readFile();
+
+		function part1(): number | string {
+			return 0;
+		}
+
+		function part2(): number | string {
+			return 0;
+		}
+
+		Solver.create()
+		.setPart1(part1)
+		.setPart2(part2)
+		.solve();
+	`.replace(/\t\t/g, "");
+
+	const directoryPath = path.join("days", formatDay(day));
+	const filePath = path.join(directoryPath, Constants.CODE_FILE_NAME);
+
+	if(!fs.existsSync(directoryPath)) {
+		fs.mkdirSync(directoryPath);
+	}
+
+	fs.writeFileSync(filePath, content);
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,6 @@
 const Constants = {
 	INPUT_FILE_NAME: "input.txt",
+	CODE_FILE_NAME: "index.ts",
 	DIST_PATH: "dist"
 }
 

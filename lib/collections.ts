9c49e59a3986ecfe358dfd349f93ccbbc44e50fb
SigmaHyperon <nano_tech@web.de>
2021-12-09 18:58:40 +0100

    add matrix type

--- lib/collections.ts
+++ lib/collections.ts
@@ -1,3 +1,5 @@
+import { Point2 } from "./geometry";
+
 export class Lists {
 	static count<T>(list: T[]): Map<T, number>;
 	static count<T, K>(list: T[], criterium?: (value: T) => K): Map<K | T, number> {
@@ -33,5 +35,56 @@ export class Sets {
 		if (set1.size !== set2.size) return false;
 		for (let a of set1) if (!set2.has(a)) return false;
 		return true;
+	}
+}
+
+export class Matrix<T> {
+	matrix: T[][];
+	height: number;
+	width: number;
+	constructor(matrix: T[][]) {
+		this.width = matrix[0].length;
+		this.height = matrix.length;
+		for(let line of matrix) {
+			if(line.length != this.width) {
+				throw "Matrix not rectangular";
+			}
+		}
+		this.matrix = matrix;
+	}
+
+	protected hasValueAt(x: number, y: number): boolean {
+		return x >= 0 && x < this.width && y >= 0 && y < this.height;
+	}
+
+	public valueAt(x: number, y: number): T | null {
+		return this.hasValueAt(x, y) ? this.matrix[y][x] : null; 
+	}
+
+	public neighbours(x: number, y: number): MatrixValue<T>[] {
+		const neighbours: MatrixValue<T>[] = [];
+		neighbours.push(new MatrixValue(x, y-1, this.valueAt(x, y-1)));
+		neighbours.push(new MatrixValue(x-1, y, this.valueAt(x - 1, y)));
+		neighbours.push(new MatrixValue(x, y+1, this.valueAt(x, y + 1)));
+		neighbours.push(new MatrixValue(x+1, y, this.valueAt(x + 1, y)));
+		return neighbours.filter(v => v.value != null);
+	}
+
+	public values(): MatrixValue<T>[] {
+		const values: MatrixValue<T>[] = [];
+		for(let y = 0; y < this.height; y++) {
+			for(let x = 0; x < this.width; x++) {
+				values.push(new MatrixValue(x, y, this.valueAt(x, y)));
+			}
+		}
+		return values;
+	}
+}
+
+export class MatrixValue<T> extends Point2 {
+	value: T;
+	constructor(x: number, y: number, value: T) {
+		super(x, y);
+		this.value = value;
 	}
 }
